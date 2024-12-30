e19400f08b592ad9dd5e22e6ead3f42970a5e7f9
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-12-30 22:05:34 +0900

    feat: Spring 프로젝트 생성

---
+++ src/main/java/com/example/junho/sns_demo/SnsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.junho.sns_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SnsDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnsDemoApplication.class, args);
+  }
+
+}
