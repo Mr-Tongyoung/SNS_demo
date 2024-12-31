28d212d502af271003c907161c3fe79b01d3577e
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-12-31 12:28:52 +0900

    feat: Security설정

---
+++ src/main/java/com/example/junho/sns_demo/global/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.junho.sns_demo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf->csrf.disable()) // CSRF 보호 비활성화
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/webjars/**",
+                "/actuator/**",
+                "/user/**"
+            ).permitAll() // Swagger 및 관련 리소스 허용
+            .anyRequest().authenticated() // 나머지 요청은 인증 필요
+        );
+
+    http.cors(cors -> cors.disable()); // 테스트를 위해 CORS 비활성화
+
+
+    return http.build();
+  }
+}
