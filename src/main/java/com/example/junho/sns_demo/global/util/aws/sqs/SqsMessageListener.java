34f4411a504c546b3f774d2bc945a3cf0aba95a4
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2025-01-22 17:50:09 +0900

    format

--- src/main/java/com/example/junho/sns_demo/global/util/aws/sqs/SqsMessageListener.java
+++ src/main/java/com/example/junho/sns_demo/global/util/aws/sqs/SqsMessageListener.java
@@ -3,6 +3,8 @@ package com.example.junho.sns_demo.global.util.aws.sqs;
 import com.example.junho.sns_demo.domain.newsFeed.service.NewsfeedUpdateService;
 import com.example.junho.sns_demo.domain.post.domain.Post;
 import com.example.junho.sns_demo.domain.post.repository.PostRepository;
+import com.example.junho.sns_demo.global.exception.CustomException;
+import com.example.junho.sns_demo.global.exception.ErrorCode;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,7 +18,8 @@ public class SqsMessageListener {
   @SqsListener("NewsfeedCacheQueue")
   public void processMessage(String message) {
     System.out.println("Listener: " + message);
-    Post post = postRepository.findById(Long.parseLong(message)).orElse(null);
+    Post post = postRepository.findById(Long.parseLong(message))
+        .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
     // 팔로워 캐시 업데이트
     System.out.println("Cache 업데이트");
     newsfeedUpdateService.updateFollowerCaches(post.getUser().getId(), post.getId());
